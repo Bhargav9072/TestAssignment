21984d479451071314864c2d9de27ccca4fc1b84
Bhargav9072 <55135498+Bhargav9072@users.noreply.github.com>
2024-04-15 20:37:03 +0530

    Add files via upload

---
+++ TestAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+try
+{
+    Console.Write("Please enter a string: ");
+    string inputValue = Console.ReadLine().ToLower();
+    bool isInvalidInput = false;
+    while (!isInvalidInput)
+    {
+        if (string.IsNullOrEmpty(inputValue))
+        {
+            Console.WriteLine("Invalid Input. Try again...");
+            Console.Write("Please enter a string: ");
+            inputValue = Console.ReadLine().ToLower();
+            isInvalidInput = false;
+        }
+        else
+        {
+            isInvalidInput = true;
+        }
+    }
+    inputValue = inputValue.ToUpper(CultureInfo.InvariantCulture);
+    var result = inputValue.GroupBy(p => p).Select(p => new { Count = p.Count(), Char = p.Key }).GroupBy(p => p.Count, p => p.Char).OrderByDescending(p => p.Key).First();
+
+    foreach (var r in result)
+    {
+        Console.WriteLine("Frequent character:{0}, count:{1}", r, result.Key);
+    }
+
+    Console.Write("Press [enter] to continue...");
+    Console.ReadLine();
+}
+catch (Exception)
+{
+    throw;
+}
